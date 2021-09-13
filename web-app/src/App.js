b92733a2eabcd01b06ccc4614407deb4a81dd6d9
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-13 18:33:55 +0400

    feat: add component

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,16 +1,11 @@
-import './App.css';
+import "./App.css";
 // Это корневой компонент
 // Сохраняйте компоненты в папке components
 // Если создаёте универсальный компонет (например кнопка) сохраняйте в папку components/generic
 // Для своих корневых компонентов можно создавать свои подпапки. Например components/products для компонета автомобиля
 
-
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- web-app/src/components/logo.js
+++ web-app/src/components/logo.js
@@ -1,4 +1,17 @@
 function Logo() {
-  return;
+  return (
+    <Logo>
+      <nav>
+        <ul>
+          <li>Автомобили</li>
+          <li>Сервис</li>
+          <li>Запчасти и аксессуары</li>
+          <li>Кузовной ремонт</li>
+          <li>Финансовые услуги</li>
+          <li>Клиентам</li>
+        </ul>
+      </nav>
+    </Logo>
+  );
 }
 export default Logo;
