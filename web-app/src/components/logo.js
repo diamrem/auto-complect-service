4cca97df96fee3a9fc288d1c6c004eb9335b96aa
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-13 19:51:11 +0400

    feat: add style for logo

--- web-app/src/components/logo.js
+++ web-app/src/components/logo.js
@@ -1,15 +1,23 @@
 function Logo() {
   return (
-    <nav>
+    <logo className="logo">
       <ul className="logo_item">
-        <a className="logo_link">Автомобили</a>
+        <a className="logo_link" href="https://atlantm.by/catalog/ford/">
+          Автомобили
+        </a>
         <a className="logo_link">Сервис</a>
         <a className="logo_link">Запчасти и аксессуары</a>
         <a className="logo_link">Кузовной ремонт</a>
         <a className="logo_link">Финансовые услуги</a>
         <a className="logo_link">Клиентам</a>
       </ul>
-    </nav>
+    </logo>
   );
 }
 export default Logo;
+
+function Image() {
+  <image>
+    <ul className="image_item"></ul>
+  </image>;
+}
